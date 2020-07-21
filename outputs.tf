6e4b1e217b507e0a8c694944e8245013edd113cb
Cas <cas.thomas.r@gmail.com>
2020-07-21 09:26:40 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 # Outputs
 
 output "cloudfront_id" {
-  value = aws_cloudfront_distribution.cf_distribution.id
+  value = aws_cloudfront_distribution.cf_distribution.0.id
 }
 
 output "cloudfront_arn" {
-  value = aws_cloudfront_distribution.cf_distribution.arn
+  value = aws_cloudfront_distribution.cf_distribution.0.arn
 }
 
 output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.cf_distribution.domain_name
+  value = aws_cloudfront_distribution.cf_distribution.0.domain_name
 }
 
 output "hosted_zone_id" {
-  value = aws_cloudfront_distribution.cf_distribution.hosted_zone_id
+  value = aws_cloudfront_distribution.cf_distribution.0.hosted_zone_id
 }
